475347d70c68f48ce0d972ab5504e9e8f12852e7
Nate Wienert <natewienert@gmail.com>
2014-02-20 17:06:38 -0800

    More fixes

--- app/views/mixins/PageData.js
+++ app/views/mixins/PageData.js
@@ -1,8 +1,10 @@
+var Router = require('../../lib/Router');
+
 module.exports = {
 
   componentWillMount: function() {
     // Set up page data structure
-    var pageData = new Cortex(this.params.data, this.updatePageData);
+    this.pageData = new Cortex(this.params.data, this.updatePageData);
   },
 
   updatePageData: function() {
