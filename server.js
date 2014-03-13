1b31262207c1afbbf27c67e18ec498b7bdd7ce7f
natew <natewienert@gmail.com>
2014-03-13 10:39:56 -0700

    Use reactor-core middleware, update version

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
-var nodeJSX       = require('node-jsx');
-    nodeJSX.install({ extension: '.jsx', additionalTransform: addPragma });
-var path          = require('path');
-var url           = require('url');
-var express       = require('express');
-var browserify    = require('connect-browserify');
-var ReactAsync    = require('react-async');
-var Server        = express();
-var webpack       = require('webpack');
-var wpMiddleware  = require('webpack-dev-middleware');
-var wpConfig      = require('./webpack.config');
-var App           = require('./app/app');
-
-function addPragma(src) {
-  return '/** @jsx React.DOM */' +  src;
-}
+var nodeJSX           = require('node-jsx').install({ extension: '.jsx' });
+var path              = require('path');
+var express           = require('express');
+var Server            = express();
+var webpack           = require('webpack');
+var wpMiddleware      = require('webpack-dev-middleware');
+var wpConfig          = require('./webpack.config');
+var reactorMiddleware = require('reactor-core/lib/middleware');
+var App               = require('./app/app');
 
 var HOST      = process.env.NODE_HOST || 'localhost';
 var ENV       = process.env.NODE_ENV || 'development';
@@ -22,38 +15,17 @@ var ASSET_DIR = '/assets';
 
 console.log('ENV:', ENV);
 
-var props = {
-  host: HOST, port: PORT, env: ENV,
-  bundle: ASSET_DIR + '/js/app.js'
-};
-
-var render = function(req, res, next) {
-  props.path = url.parse(req.url).pathname;
-  ReactAsync.renderComponentToStringWithAsyncState(
-    App(props),
-    function(err, markup, data) {
-      if (err) return next(err);
-      markup = ReactAsync.injectIntoMarkup(markup, data, [props.bundle])
-      res.send('<!doctype>' + markup);
-    }
-  );
-}
-
 var api = function(req, res) {
   try { var controller = require('./api/' + req.params.controller); }
-  catch(e) {
-    console.log('error', e);
-    res.send(500, e);
-  }
+  catch(e) { console.log('error', e); res.send(500, e); }
   var params = req.params;
   if (params.name) controller = controller[params.controller][params.name];
   res.json(controller);
 };
 
 // Serve JS bundle in dev
-if (props.env == 'development') {
+if (ENV == 'development')
   Server.use(wpMiddleware(webpack(wpConfig), { publicPath: ASSET_DIR+'/js' }));
-}
 
 Server
   .use(ASSET_DIR, express.static(path.join(__dirname, 'build')))
@@ -61,7 +33,15 @@ Server
   .use('/bower', express.static(path.join(__dirname, 'bower_components')))
   .use('/images', express.static(path.join(__dirname, 'app'+ASSET_DIR+'/images')))
   .use(express.favicon())
-  .use(render)
-  .listen(props.port);
+  .use(reactorMiddleware({
+    app: App,
+    props: {
+      host: HOST,
+      port: PORT,
+      env: ENV,
+      bundle: ASSET_DIR + '/js/app.js'
+    }
+  }))
+  .listen(PORT);
 
 console.log('Server started at http://' + HOST + ':' + PORT);
